b59bdb00b62b4c62ec89062bdec6324bc056ca7b
nandana <nandanaunni65@gmail.com>
2024-09-06 12:13:55 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function showMenu() {
+  document.getElementById("logslink").style.right = "0";
+}
+
+function hideMenu() {
+  document.getElementById("logslink").style.right = "-100%";
+}
---
+++ mainc.js
@@ -0,0 +1,9 @@
+function showMenu() {
+  const menu = document.getElementById("logslink");
+  menu.style.right = "0";
+}
+
+function hideMenu() {
+  const menu = document.getElementById("logslink");
+  menu.style.right = "-100%";
+}
